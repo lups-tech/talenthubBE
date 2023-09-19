5fe6e8775405be4b4370206389934aa5f60b75d6
SMooreSwe <stevedm88@gmail.com>
2023-09-19 15:18:12 +0200

    removed unnecessary using statements

--- Data/Repositories/Users/UserRepository.cs
+++ Data/Repositories/Users/UserRepository.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 using talenthubBE.Mapping;
 using talenthubBE.Models;
 using talenthubBE.Models.Users;
