2bcc9a7be5129c08fe5fc017f3621e74b8a083b7
SMooreSwe <stevedm88@gmail.com>
2023-10-22 14:20:05 +0200

    added migration and database update

--- Migrations/20231022121815_NewUserAndJob.Designer.cs
+++ Migrations/20231022121815_NewUserAndJob.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace talenthubBE.Migrations
 {
     [DbContext(typeof(MvcDataContext))]
-    partial class MvcDataContextModelSnapshot : ModelSnapshot
+    [Migration("20231022121815_NewUserAndJob")]
+    partial class NewUserAndJob
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
